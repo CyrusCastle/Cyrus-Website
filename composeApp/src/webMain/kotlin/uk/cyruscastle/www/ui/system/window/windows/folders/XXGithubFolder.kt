e7a005cd7bad9ffe888d55e5856b5ac254ac406a
BosunCyrus <81359374+BosunCyrus@users.noreply.github.com>
2026-04-29 21:54:33 +0100

    Add DrawBox-Enhanced to my Github Project list

--- composeApp/src/webMain/kotlin/uk/cyruscastle/www/ui/system/window/windows/folders/XXGithubFolder.kt
+++ composeApp/src/webMain/kotlin/uk/cyruscastle/www/ui/system/window/windows/folders/XXGithubFolder.kt
@@ -1,6 +1,7 @@
 package uk.cyruscastle.www.ui.system.window.windows.folders
 
 import cyruswebsite.composeapp.generated.resources.Res
+import cyruswebsite.composeapp.generated.resources.internetExplorerHTML
 import cyruswebsite.composeapp.generated.resources.scriptFolder
 import cyruswebsite.composeapp.generated.resources.scriptYellow
 import uk.cyruscastle.www.ui.system.window.windows.shortcuts.ShortcutWindow
@@ -8,11 +9,17 @@ import uk.cyruscastle.www.ui.system.window.windows.shortcuts.ShortcutWindow
 class XXGithubFolder : FileExplorerWindow(
     title = "GitHub Projects",
     folderIcon = Res.drawable.scriptFolder,
-    items = listOf(ZZCyrusWebsiteGithub())
+    items = listOf(ZZCyrusWebsiteGithub(), ZZDrawBoxEnhancedGitHubShortcut())
 )
 
 class ZZCyrusWebsiteGithub : ShortcutWindow(
     title = "This Website",
     shortcutIcon = Res.drawable.scriptYellow,
     websiteURL = "https://github.com/CyrusCastle/Cyrus-Website/"
+)
+
+class ZZDrawBoxEnhancedGitHubShortcut : ShortcutWindow(
+    title = "DrawBox Enhanced",
+    shortcutIcon = Res.drawable.scriptYellow,
+    websiteURL = "https://github.com/CyrusCastle/DrawBox-Enhanced"
 )
