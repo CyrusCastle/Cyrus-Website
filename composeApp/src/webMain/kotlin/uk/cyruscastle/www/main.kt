b1240558560bafe141384f205ef31503dbd3223e
BosunCyrus <81359374+BosunCyrus@users.noreply.github.com>
2026-06-18 22:05:25 +0100

    New context menu for KMP

--- composeApp/src/webMain/kotlin/uk/cyruscastle/www/main.kt
+++ composeApp/src/webMain/kotlin/uk/cyruscastle/www/main.kt
@@ -1,13 +1,17 @@
 package uk.cyruscastle.www
 
+import androidx.compose.foundation.ComposeFoundationFlags
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import uk.cyruscastle.www.ui.system.desktop.WindowsDesktop
 import uk.cyruscastle.www.ui.theme.font.WindowsTypography
 
-@OptIn(ExperimentalComposeUiApi::class)
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalFoundationApi::class)
 fun main() {
+    ComposeFoundationFlags.isNewContextMenuEnabled = true
+
     ComposeViewport {
         MaterialTheme(typography = WindowsTypography()) {
             WindowsDesktop()
