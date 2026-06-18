edc675b9698a44ee482c93b338ae524ff1e8f8e7
BosunCyrus <81359374+BosunCyrus@users.noreply.github.com>
2026-06-18 22:09:34 +0100

    Move Cawlfythol into one folder

--- composeApp/src/webMain/kotlin/uk/cyruscastle/www/ui/system/window/windows/folders/XXCawlfytholFolder.kt
+++ composeApp/src/webMain/kotlin/uk/cyruscastle/www/ui/system/window/windows/folders/XXCawlfytholFolder.kt
@@ -1,9 +1,11 @@
 package uk.cyruscastle.www.ui.system.window.windows.folders
 
 import cyruswebsite.composeapp.generated.resources.Res
+import cyruswebsite.composeapp.generated.resources.cawlfythol
 import cyruswebsite.composeapp.generated.resources.cawlfytholFolder
-import uk.cyruscastle.www.ui.system.window.windows.html.edge.ZZPuckNCoverSite
-import uk.cyruscastle.www.ui.system.window.windows.shortcuts.ZZPuckNCoverGitupShortcut
+import cyruswebsite.composeapp.generated.resources.internetExplorerHTML
+import uk.cyruscastle.www.ui.system.window.windows.html.edge.BrowserWindow
+import uk.cyruscastle.www.ui.system.window.windows.shortcuts.ShortcutWindow
 
 class XXCawlfytholFolder : FileExplorerWindow(
     title = "Cawlfythol",
@@ -12,4 +14,16 @@ class XXCawlfytholFolder : FileExplorerWindow(
         ZZPuckNCoverSite(),
         ZZPuckNCoverGitupShortcut()
     )
+)
+
+class ZZPuckNCoverSite : BrowserWindow(
+    websiteName = "PuckNCover",
+    shortcutIcon = Res.drawable.cawlfythol,
+    websiteURL = "http://localhost:8080/composeResources/cyruswebsite.composeapp.generated.resources/files/samplepage.html"
+)
+
+class ZZPuckNCoverGitupShortcut : ShortcutWindow(
+    title = "PuckNCover Github",
+    shortcutIcon = Res.drawable.internetExplorerHTML,
+    websiteURL = "https://github.com/CyrusCastle/Puck-n-Cover/"
 )
