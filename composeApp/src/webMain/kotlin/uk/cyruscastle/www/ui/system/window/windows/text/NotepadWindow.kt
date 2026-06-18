f574dbde716a56db63bdbb86dcb3889c133962e8
BosunCyrus <81359374+BosunCyrus@users.noreply.github.com>
2026-06-18 22:05:48 +0100

    Add some functionality to the NotepadWindow

--- composeApp/src/webMain/kotlin/uk/cyruscastle/www/ui/system/window/windows/text/NotepadWindow.kt
+++ composeApp/src/webMain/kotlin/uk/cyruscastle/www/ui/system/window/windows/text/NotepadWindow.kt
@@ -2,26 +2,33 @@ package uk.cyruscastle.www.ui.system.window.windows.text
 
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 import cyruswebsite.composeapp.generated.resources.Res
 import cyruswebsite.composeapp.generated.resources.notepad
 import cyruswebsite.composeapp.generated.resources.txt
+import io.github.vinceglb.filekit.FileKit
+import io.github.vinceglb.filekit.dialogs.openFilePicker
+import io.github.vinceglb.filekit.download
+import io.github.vinceglb.filekit.readString
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import uk.cyruscastle.www.ui.system.scroll.ScrollBarType
 import uk.cyruscastle.www.ui.system.scroll.ScrollableContainer
 import uk.cyruscastle.www.ui.system.window.FacsimileWindow
-import uk.cyruscastle.www.ui.system.window.topbar.WindowTopBarDefaultMenus
 import uk.cyruscastle.www.ui.system.window.topbar.WindowTopBarMenuItem
 import uk.cyruscastle.www.ui.system.window.topbar.WindowTopBarMenuSubItemEntry
 import uk.cyruscastle.www.ui.system.window.topbar.WindowTopBarMenus
+import kotlin.js.ExperimentalWasmJsInterop
+import kotlin.js.js
 
 open class NotepadWindow(
     val title: String? = null,
     val startingText: String? = null,
-    val isFile: Boolean = false
+    val isFile: Boolean = false,
+    var state: MutableState<String?> = mutableStateOf(startingText)
 ) : FacsimileWindow(
     programTitle = "Notepad",
     fileTitle = title,
@@ -33,11 +40,26 @@ open class NotepadWindow(
                 WindowTopBarMenuItem(
                     "File",
                     listOf(
-                        WindowTopBarMenuSubItemEntry("New", false) {},
-                        WindowTopBarMenuSubItemEntry("Open", false) {},
-                        WindowTopBarMenuSubItemEntry("Save", false) {},
-                        WindowTopBarMenuSubItemEntry("Save As", false) {},
-                        WindowTopBarMenuSubItemEntry("Print", false) {},
+                        WindowTopBarMenuSubItemEntry("New", true) { state.value = "" },
+                        WindowTopBarMenuSubItemEntry("Open", true) {
+                            CoroutineScope(Dispatchers.Default).launch {
+                                val file = FileKit.openFilePicker()
+                                state.value = (file?.readString() ?: "Sorry, I can't load that! Try a text file.")
+                            }
+                        },
+                        WindowTopBarMenuSubItemEntry("Save", true) {
+                            CoroutineScope(Dispatchers.Default).launch {
+                                state.value?.encodeToByteArray()?.let { FileKit.download(bytes = it, fileName = "Untitled.txt") }
+                            }
+                        },
+                        WindowTopBarMenuSubItemEntry("Save As", true) {
+                            CoroutineScope(Dispatchers.Default).launch {
+                                state.value?.encodeToByteArray()?.let { FileKit.download(bytes = it, fileName = "Untitled.txt") }
+                            }
+                        },
+                        WindowTopBarMenuSubItemEntry("Print", true) {
+                            printText(state.value ?: "")
+                        },
                     )
                 ),
 
@@ -71,12 +93,10 @@ open class NotepadWindow(
         )
     }),
     content = {
-        var text by remember { mutableStateOf(startingText ?: "") }
-
         ScrollableContainer(ScrollBarType.all(), behindContentColor = Color.White) { modifier ->
             TextField(
-                text,
-                { text = it },
+                state.value ?: "",
+                { state.value = it },
                 modifier = modifier,
 
                 singleLine = false,
@@ -114,3 +134,31 @@ open class NotepadWindow(
 //        )
     }
 )
+
+@OptIn(ExperimentalWasmJsInterop::class)
+private fun printText(text: String){
+    js("""
+    const iframe = document.createElement('iframe');
+    iframe.style.position = 'fixed';
+    iframe.style.right = '0';
+    iframe.style.bottom = '0';
+    iframe.style.width = '0';
+    iframe.style.height = '0';
+    iframe.style.border = '0';
+    
+    document.body.appendChild(iframe);
+    
+    iframe.onload = function () {
+        iframe.contentWindow.focus();
+        iframe.contentWindow.print();
+    
+        // Cleanup after printing
+        setTimeout(() => {
+            document.body.removeChild(iframe);
+        }, 1000);
+    };
+    
+    
+    iframe.srcdoc = '<p>' + text + '</p>';
+    """)
+}
