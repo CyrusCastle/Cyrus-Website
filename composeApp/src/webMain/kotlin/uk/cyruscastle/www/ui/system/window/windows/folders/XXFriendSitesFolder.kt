a4eb83d249855ffa1cb440a3b667a81bb7db5e1c
BosunCyrus <81359374+BosunCyrus@users.noreply.github.com>
2026-07-29 00:10:54 +0100

    Add alex's site

--- composeApp/src/webMain/kotlin/uk/cyruscastle/www/ui/system/window/windows/folders/XXFriendSitesFolder.kt
+++ composeApp/src/webMain/kotlin/uk/cyruscastle/www/ui/system/window/windows/folders/XXFriendSitesFolder.kt
@@ -1,6 +1,7 @@
 package uk.cyruscastle.www.ui.system.window.windows.folders
 
 import cyruswebsite.composeapp.generated.resources.Res
+import cyruswebsite.composeapp.generated.resources.alex
 import cyruswebsite.composeapp.generated.resources.emily
 import cyruswebsite.composeapp.generated.resources.globeFolder
 import uk.cyruscastle.www.ui.system.window.windows.shortcuts.ShortcutWindow
@@ -8,7 +9,13 @@ import uk.cyruscastle.www.ui.system.window.windows.shortcuts.ShortcutWindow
 class XXFriendSitesFolder : FileExplorerWindow(
     title = "Friends' Sites",
     folderIcon = Res.drawable.globeFolder,
-    items = listOf(ZZEmilyShortcut())
+    items = listOf(ZZAlexShortcut(), ZZEmilyShortcut())
+)
+
+class ZZAlexShortcut : ShortcutWindow(
+    title = "Alex's Site",
+    shortcutIcon = Res.drawable.alex,
+    websiteURL = "https://a-doye.github.io/" //https://a-doye.io
 )
 
 class ZZEmilyShortcut : ShortcutWindow(
