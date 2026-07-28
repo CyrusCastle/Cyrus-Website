8840e0afb41d658311107a326ec10031c48cf7db
BosunCyrus <81359374+BosunCyrus@users.noreply.github.com>
2026-07-28 23:59:13 +0100

    Give emily's shortcut a custom icon

--- composeApp/src/webMain/kotlin/uk/cyruscastle/www/ui/system/window/windows/folders/XXFriendSitesFolder.kt
+++ composeApp/src/webMain/kotlin/uk/cyruscastle/www/ui/system/window/windows/folders/XXFriendSitesFolder.kt
@@ -1,11 +1,18 @@
 package uk.cyruscastle.www.ui.system.window.windows.folders
 
 import cyruswebsite.composeapp.generated.resources.Res
+import cyruswebsite.composeapp.generated.resources.emily
 import cyruswebsite.composeapp.generated.resources.globeFolder
-import uk.cyruscastle.www.ui.system.window.windows.shortcuts.ZZEmilyShortcut
+import uk.cyruscastle.www.ui.system.window.windows.shortcuts.ShortcutWindow
 
 class XXFriendSitesFolder : FileExplorerWindow(
     title = "Friends' Sites",
     folderIcon = Res.drawable.globeFolder,
     items = listOf(ZZEmilyShortcut())
+)
+
+class ZZEmilyShortcut : ShortcutWindow(
+    title = "Emily's Site",
+    shortcutIcon = Res.drawable.emily,
+    websiteURL = "https://emilyprust.com"
 )
