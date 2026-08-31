f56c264999e0db320267c322538897266f5bdec7
BosunCyrus <81359374+BosunCyrus@users.noreply.github.com>
2026-08-31 01:39:12 +0100

    quick colour palette

---
+++ mobileWebApp/src/webMain/kotlin/uk/cyruscastle/www/view/ColorPalette.kt
@@ -0,0 +1,18 @@
+package uk.cyruscastle.www.view
+
+import androidx.compose.ui.graphics.Color
+
+object ColorPalette { // TODO lets standardise this a bit
+    val CaseDark = Color(0xFF2B2B2D)
+    val CaseMid = Color(0xFF454548)
+    val CaseLight = Color(0xFF5E5E63)
+    val CaseEdge = Color(0xFF17171A)
+    val KeyTop = Color(0xFF3A3A3E)
+    val KeyBottom = Color(0xFF232326)
+    val KeyText = Color(0xFFE8E8E8)
+    val KeySubText = Color(0xFF9A9A9E)
+    val NavBlue = Color(0xFF3E5C82)
+    val ScreenGreen = Color(0xFF8FBE3F)
+    val CallGreen = Color(0xFF4CAF50)
+    val EndRed = Color(0xFFE04B3A)
+}
