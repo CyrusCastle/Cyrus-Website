aac1960445e7c33eccb59811749cf865b62a46a8
BosunCyrus <81359374+BosunCyrus@users.noreply.github.com>
2026-09-10 19:42:50 +0100

    Sort out Typography for the phone

--- mobileWebApp/src/webMain/kotlin/uk/cyruscastle/www/main.kt
+++ mobileWebApp/src/webMain/kotlin/uk/cyruscastle/www/main.kt
@@ -5,14 +5,17 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
+import cyruswebsite.shared.generated.resources.NokiaSansRetro
+import cyruswebsite.shared.generated.resources.Res
 import uk.cyruscastle.www.view.phone.NokiaN70
+import uk.cyruscastle.www.view.typography
 
 @OptIn(ExperimentalComposeUiApi::class, ExperimentalFoundationApi::class)
 fun main() {
     ComposeFoundationFlags.isNewContextMenuEnabled = true
 
     ComposeViewport {
-        MaterialTheme(/*typography = WindowsTypography()*/) {
+        MaterialTheme(typography = typography(Res.font.NokiaSansRetro)) {
             NokiaN70()
         }
     }
