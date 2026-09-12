4ff319940982b551bc70dfbcc0c1fe578895d663
BosunCyrus <81359374+BosunCyrus@users.noreply.github.com>
2026-09-12 15:28:57 +0100

    Add the PDFs from main site

--- mobileWebApp/src/webMain/kotlin/uk/cyruscastle/www/view/screen/folder/XXMiscDocsFolder.kt
+++ mobileWebApp/src/webMain/kotlin/uk/cyruscastle/www/view/screen/folder/XXMiscDocsFolder.kt
@@ -2,9 +2,20 @@ package uk.cyruscastle.www.view.screen.folder
 
 import cyruswebsite.shared.generated.resources.Res
 import cyruswebsite.shared.generated.resources.phoneFolderTwo
+import uk.cyruscastle.www.view.screen.dynamic.pdf.PdfViewerApp
 
 class XXMiscDocsFolder : FolderViewer(
     name = "Misc",
-    apps = listOf(),
+    apps = listOf(ZZCVPdfWindow(), ZZWelshForSoldiersWindow()),
     icon = Res.drawable.phoneFolderTwo
+)
+
+class ZZWelshForSoldiersWindow : PdfViewerApp(
+    pdfTitle = "welsh for the discerning soldier.pdf",
+    pdfFilePath = "wfds.pdf"
+)
+
+class ZZCVPdfWindow : PdfViewerApp(
+    pdfTitle = "Cyrus CV.pdf",
+    pdfFilePath = "cv.pdf"
 )
--- mobileWebApp/src/webMain/kotlin/uk/cyruscastle/www/view/screen/folder/XXPublicationsFolder.kt
+++ mobileWebApp/src/webMain/kotlin/uk/cyruscastle/www/view/screen/folder/XXPublicationsFolder.kt
@@ -2,9 +2,15 @@ package uk.cyruscastle.www.view.screen.folder
 
 import cyruswebsite.shared.generated.resources.Res
 import cyruswebsite.shared.generated.resources.phoneFolderTwo
+import uk.cyruscastle.www.view.screen.dynamic.pdf.PdfViewerApp
 
 class XXPublicationsFolder : FolderViewer(
     name = "Publications",
-    apps = listOf(),
+    apps = listOf(ZZwje766()),
     icon = Res.drawable.phoneFolderTwo
+)
+
+class ZZwje766 : PdfViewerApp(
+    pdfTitle = "Policy for GenAI in Wales.pdf",
+    pdfFilePath = "wje766.pdf"
 )
