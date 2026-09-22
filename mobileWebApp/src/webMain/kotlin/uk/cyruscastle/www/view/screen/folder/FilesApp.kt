c24f3ede0fa85e07a1723577012c5a34364e79b4
BosunCyrus <81359374+BosunCyrus@users.noreply.github.com>
2026-09-22 17:30:19 +0100

    Hide friend folder

--- mobileWebApp/src/webMain/kotlin/uk/cyruscastle/www/view/screen/folder/FilesApp.kt
+++ mobileWebApp/src/webMain/kotlin/uk/cyruscastle/www/view/screen/folder/FilesApp.kt
@@ -2,5 +2,5 @@ package uk.cyruscastle.www.view.screen.folder
 
 class FilesApp : FolderViewer(
     name = "Files",
-    apps = listOf(XXFriendFolder(), XXGithubFolder(), XXPublicationsFolder(), XXMiscDocsFolder())
+    apps = listOf(/*XXFriendFolder(),*/ XXGithubFolder(), XXPublicationsFolder(), XXMiscDocsFolder())
 )
