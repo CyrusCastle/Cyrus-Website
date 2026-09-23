97147cd4fe3a612046728ec9fe3bf7b12e6b130a
BosunCyrus <81359374+BosunCyrus@users.noreply.github.com>
2026-09-23 20:53:50 +0100

    Slightly improve default msg

--- mobileWebApp/src/webMain/kotlin/uk/cyruscastle/www/controller/HelpManager.kt
+++ mobileWebApp/src/webMain/kotlin/uk/cyruscastle/www/controller/HelpManager.kt
@@ -8,4 +8,4 @@ object HelpManager {
     var message = mutableStateOf<HelpMessage?>(null)
 }
 
-const val DEFAULT_MESSAGE_SHORT = "Welcome to my personal/professional website! My main site doesn't scale well for mobile, so this version will have to do."
+const val DEFAULT_MESSAGE_SHORT = "Welcome to my website! My main site can't scale well for mobile, so here's a limited 'mobile version'."
